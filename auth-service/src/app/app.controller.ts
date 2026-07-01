f303ea5ae01a8b579f53b578c1250250733f3e1c
Jawad Ali <149782239+jawadDev1@users.noreply.github.com>
2026-07-01 12:48:48 +0500

    feat: dead letter queue

--- auth-service/src/app/app.controller.ts
+++ auth-service/src/app/app.controller.ts
@@ -1,10 +1,35 @@
-import { Controller } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { Controller, Inject, OnModuleInit } from '@nestjs/common';
+import { ClientKafka, EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller()
-export class AppController {
+export class AppController implements OnModuleInit {
+  constructor(
+    @Inject('AUTH_SERVICE') private readonly authClient: ClientKafka,
+  ) {}
+
+  async onModuleInit() {
+    await this.authClient.connect();
+  }
+
   @EventPattern('session_updated')
   handleUserSession(@Payload() data: { name: string }) {
-    console.log('Event Recieved', data);
+    try {
+      console.log('Event Recieved', data);
+
+      throw new Error('Session Update failed');
+    } catch (error) {
+      const msg = error instanceof Error ? error.message : String(error);
+      this.authClient.emit('failed_session_update_dlq', msg);
+
+      return {
+        success: false,
+        message: msg,
+      };
+    }
+  }
+
+  @EventPattern('failed_session_update_dlq')
+  handleFailedSessionUpdate(@Payload() msg: string) {
+    console.log('Received failed message :: ', msg);
   }
 }
